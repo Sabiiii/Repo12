213fe03d851c36b2c65860a8d5165b455f3b8268
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-25 11:34:09 +0400

    Adding nested arrays

---
+++ ex05/src/index.js
@@ -0,0 +1,36 @@
+myPetsArray = [
+    {
+    animalType: "Dog",
+    name: ["Pujdo"]
+    },
+    {
+    animalType: "Cat",
+    name: ["Maca"]
+    },
+    {
+    animalType: "Bird",
+    name: ["Tweety"]
+
+    }
+        
+    ];
+
+    
+
+function myPetsFunction(pets) {
+
+   
+    myPetsArray[1].name[0];
+
+
+}
+
+
+
+console.log(myPetsFunction(myPetsArray));
+module.exports = myPetsFunction;
+module.exports.myPets = myPetsArray;
+
+
+
+
